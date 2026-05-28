854c37a736049408701625f34a62d1d63e031541
siddhardhalla09 <siddhardhalla09@gmail.com>
2026-05-28 11:11:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.ORDER_APP_CONFIG = {
-  backendUrl: "https://script.google.com/macros/s/AKfycbwoBRTVGcrgKNGT8HegPnVnWu-98Guxrxs9bE_0sTqrmuwPMVf63l1sXoyLCU6g5I0vSw/exec",
+  backendUrl: "https://script.google.com/macros/s/AKfycbwmCvW65uB9QT8OEDHZU6eiBXDBPlqg1c9a3fEwmdp0QwN2LC5ZMNV7MzaKTcvphRL2fA/exec",
   businessPhoneDigits: "763-900-6635",
   businessEmail: "siddhardha.alla@gmail.com",
   localStorageKey: "indianBazaarOrders"
