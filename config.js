35c91e815657606cc0bc20ee91b442db31590aa5
siddhardhalla09 <siddhardhalla09@gmail.com>
2026-05-28 10:24:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.ORDER_APP_CONFIG = {
-  backendUrl: "",
+  backendUrl: "https://script.google.com/macros/s/AKfycbwDQwIc8kE1t3lETSryIuIhU7ixglHa1raccD-shHyRkx48fW8AUSZfQHF3TZV6bnZ6/exec",
   businessPhoneDigits: "15551234567",
   businessEmail: "orders@indianbazaar.example",
   localStorageKey: "indianBazaarOrders"
